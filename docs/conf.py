2a95d0276efe374b2da103bb6c882cfb6cf97c32
Ritvik Nag <nagrim7@gmail.com>
2024-11-06 00:15:05 -0500

    update workflow

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -49,7 +49,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = ['.rst', '.md']
+source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
